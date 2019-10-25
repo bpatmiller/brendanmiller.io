300fc69b745a1c6fb56780089824d946853d6d15
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-10-25 14:37:47 -0500

    wow things r happening

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './NavBar.scss'
+
+export default class NavBar extends Component {
+    render() {
+        return(
+            <div className='navBar-wrapper'>
+            <span className='navBar-col navBar-left'>
+                <span className='navBar-span'>brendan miller</span>
+                <span className='navBar-span'>im gay</span>
+            </span>
+            <span className='navBar-col navBar-right'>
+                <span className='navBar-span'>right align</span>
+            </span>
+            </div>
+        );
+    }
+};
