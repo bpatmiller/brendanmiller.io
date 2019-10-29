d774015ead47080bacc76b3f227a806fe86bd922
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-10-29 15:32:16 -0500

    follows mouse

--- src/components/ContentGridPanel.js
+++ src/components/ContentGridPanel.js
@@ -3,13 +3,16 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { useHover } from "./UseHover";
 import {useSpring, animated} from 'react-spring'
+import { useMousePosition } from "./UseMousePosition";
 
 import "./ContentGrid.scss";
 
+const AnimatedPaper = animated(Paper);
+
 export default function ContentGridPanel(props) {
   const [hoverRef, isHovered] = useHover();
-  const springProps = useSpring({transform: isHovered ? 'translate(5px, -5px)' : 'translate(0px, 0px)'})
-  const AnimatedPaper = animated(Paper);
+  const mousePosition = useMousePosition();
+  const springProps = useSpring({transform: isHovered ? getTranslation(hoverRef, isHovered, mousePosition) : 'translate(0px, 0px)'})
 
   return (
     <Grid item xs={12} sm={6} md={4}>
@@ -17,3 +20,17 @@ export default function ContentGridPanel(props) {
     </Grid>
   );
 }
+
+function getTranslation(hoverRef, isHovered, mousePosition) {
+    const node = hoverRef.current;
+    if (! node || ! isHovered) {
+      return 'translate(0px,0px)';
+    }
+    const bbRect = node.getBoundingClientRect();
+    const centerPosition = { x: (bbRect.right + bbRect.left) / 2, y: (bbRect.top + bbRect.bottom)/2 };
+    const displacement = {x: (mousePosition.x - centerPosition.x) / 10, y: ( mousePosition.y - centerPosition.y) / 10 };
+    const translationString = "translate("+ displacement.x.toString() + "px," + displacement.y.toString() + "px)";
+
+  return translationString;
+}
+
