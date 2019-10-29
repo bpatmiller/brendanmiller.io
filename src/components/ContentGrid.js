5ab5bb472ebef2626e887253db18c987dbbbccd8
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-10-29 15:24:47 -0500

    spring gang

--- src/components/ContentGrid.js
+++ src/components/ContentGrid.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ContentGridPanel from './ContentGridPanel'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
+import './ContentGrid.scss'
 
 var array1 = ['panel1', 'panel2', 'panel3', 'panel4', 'panel5', 'panel6', 'panel7', 'panel8', 'panel9'];
 
@@ -20,10 +10,8 @@ var array1 = ['panel1', 'panel2', 'panel3', 'panel4', 'panel5', 'panel6', 'panel
 const gridItems = array1.map((x) => <ContentGridPanel title={x}/>);
 
 export default function ContentGrid(props) {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div className='root'>
       <Grid container spacing={3}>
         {gridItems}
       </Grid>
--- src/components/ContentGridPanel.js
+++ src/components/ContentGridPanel.js
@@ -1,34 +1,19 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import './ContentGridPanel.scss'
-import './UseMousePosition'
-import { useMousePosition } from './UseMousePosition';
-import { useHover } from './UseHover';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { useHover } from "./UseHover";
+import {useSpring, animated} from 'react-spring'
+
+import "./ContentGrid.scss";
 
 export default function ContentGridPanel(props) {
-    const [hoverRef, isHovered] = useHover();
-    const mousePosition = useMousePosition();
-    const translateString = getTranslateString(hoverRef, isHovered, mousePosition);
+  const [hoverRef, isHovered] = useHover();
+  const springProps = useSpring({transform: isHovered ? 'translate(5px, -5px)' : 'translate(0px, 0px)'})
+  const AnimatedPaper = animated(Paper);
 
-    return (
+  return (
     <Grid item xs={12} sm={6} md={4}>
-        <Paper className='paper' style={isHovered ? {  transform: translateString } : {}} ref={hoverRef}>{isHovered ? props.title : props.title}</Paper>
+        <AnimatedPaper className="paper" style={springProps}  ref={hoverRef}> {isHovered? "hover" : props.title}</AnimatedPaper>
     </Grid>
-    );
-}
-
-function getTranslateString(hoverRef, isHovered, mousePosition) {
-  const node = hoverRef.current;
-  if (! node || ! isHovered) {
-    return '';
-  }
-
-  const bbRect = node.getBoundingClientRect();
-  const centerPosition = { x: (bbRect.right + bbRect.left) / 2, y: (bbRect.top + bbRect.bottom)/2 };
-  const displacement = {x: (mousePosition.x - centerPosition.x) / 15, y: ( mousePosition.y - centerPosition.y) / 15 };
-  const translationString = "translate("+ displacement.x.toString() + "px," + displacement.y.toString() + "px)";
-
-  return translationString;
- 
+  );
 }
