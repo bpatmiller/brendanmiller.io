73b97c4c128c8bf3081bbde0e9c30a5103f716cc
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-10-29 11:12:45 -0500

    transitions finally work

--- src/components/ContentGridPanel.js
+++ src/components/ContentGridPanel.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { useRef, useState, useEffect } from 'react';
-import './ContentGridPanel.css'
+import './ContentGridPanel.scss'
+import './UseMousePosition'
+import { useMousePosition } from './UseMousePosition';
+import { useHover } from './UseHover';
 
 export default function ContentGridPanel(props) {
     const [hoverRef, isHovered] = useHover();
+    const mousePosition = useMousePosition();
+    const translateString = getTranslateString(hoverRef, isHovered, mousePosition);
 
     return (
     <Grid item xs={12} sm={6} md={4}>
-        <Paper className='paper' ref={hoverRef}>{isHovered ? 'hovered' : props.title}</Paper>
+        <Paper className='paper' style={isHovered ? {  transform: translateString } : {}} ref={hoverRef}>{isHovered ? props.title : props.title}</Paper>
     </Grid>
     );
 }
 
-function useHover() {
-  const [value, setValue] = useState(false);
+function getTranslateString(hoverRef, isHovered, mousePosition) {
+  const node = hoverRef.current;
+  if (! node || ! isHovered) {
+    return '';
+  }
 
-  const ref = useRef(null);
+  const bbRect = node.getBoundingClientRect();
+  const centerPosition = { x: (bbRect.right + bbRect.left) / 2, y: (bbRect.top + bbRect.bottom)/2 };
+  const displacement = {x: (mousePosition.x - centerPosition.x) / 15, y: ( mousePosition.y - centerPosition.y) / 15 };
+  const translationString = "translate("+ displacement.x.toString() + "px," + displacement.y.toString() + "px)";
 
-  const handleMouseOver = () => setValue(true);
-  const handleMouseOut = () => setValue(false);
-
-  useEffect(
-    () => {
-      const node = ref.current;
-      if (node) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        };
-      }
-    },
-  );
-
-  return [ref, value];
+  return translationString;
+ 
 }
