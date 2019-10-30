5879df1f0190d972545a3da1b59ec2db8ad8d7d7
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-10-30 15:57:43 -0500

    format navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import './NavBar.scss'
-import ColorBar from './ColorBar.js'
+import React, { Component } from "react";
+import "./NavBar.scss";
+import ColorBar from "./ColorBar.js";
 
 export default class NavBar extends Component {
-    render() {
-        return(
-            <div className='navBar-wrapper'>
-            <span className='navBar-col navBar-left'>
-                <span className='navBar-span'>brendan miller</span>
-                <span className='navBar-span'>example text<br/>im gay</span>
-            </span>
-            <span className='navBar-col navBar-center'>
-            <span className='navBar-span'>
-                <ColorBar/>
-            </span>
-            </span>
-            <span className='navBar-col navBar-right'>
-                <span className='navBar-span'>right align</span>
-            </span>
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div className="navBar-wrapper">
+        <span className="navBar-col navBar-left">
+          <span className="navBar-span">brendan miller</span>
+          <span className="navBar-span">
+            example text
+            <br />
+            im gay
+          </span>
+        </span>
+        <span className="navBar-col navBar-center">
+          <span className="navBar-span">
+            <ColorBar />
+          </span>
+        </span>
+        <span className="navBar-col navBar-right">
+          <span className="navBar-span">right align</span>
+        </span>
+      </div>
+    );
+  }
+}
