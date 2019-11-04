8029b4fd2edc5c6132e56ce35027cdfee32dba28
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-11-04 15:08:42 -0600

    added 404 page, fixed colors

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,6 +1,20 @@
 import React from "react";
-import "../App.scss";
+import { Link } from "react-router-dom";
 
-export default function TestPage() {
-  return <div className="Page">404</div>;
+import "../App.scss";
+import "../components/404.scss";
+
+export default function TestPage(props) {
+  var currentLocation = props.location.pathname;
+
+  return (
+    <div className="Page404">
+      <div className="Block404">
+      404<br/>
+      <span className="Hilight404">{currentLocation}</span> not found
+      <br/><br/>
+      return <Link to="/">home</Link>
+      </div>
+    </div>
+  );
 }
