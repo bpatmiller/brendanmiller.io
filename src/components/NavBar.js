92b553c968acf6db744d5266703f5eb7137001e6
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-11-06 14:39:21 -0600

    styling, edit navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,14 +8,14 @@ export default class NavBar extends Component {
     return (
       <div className="navBar-wrapper">
         <span className="navBar-col navBar-left">
-          <span className="navBar-span">brendan miller</span>
-          <span className="navBar-span">
-            example text
-            <br />
-            im gay
-          </span>
+          <span className="navBar-span">brendan<br/>miller</span>
           <span className="navBar-span">
             <ColorBar />
+          </span>
+          <span className="navBar-span">
+            software engineer
+            <br />
+            graphics / machine learning / physical simulation / fractals
           </span>
         </span>
         <span className="navBar-col navBar-right">
