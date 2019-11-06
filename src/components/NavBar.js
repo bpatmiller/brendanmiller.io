ac94be303603874df0853abb0a6d3d1560b464c7
Brendan Miller <brendan@Brendans-MacBook-Pro.local>
2019-11-06 15:05:39 -0600

    icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ColorBar from "./ColorBar.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import "../scss/NavBar.scss";
 
@@ -8,7 +10,11 @@ export default class NavBar extends Component {
     return (
       <div className="navBar-wrapper">
         <span className="navBar-col navBar-left">
-          <span className="navBar-span">brendan<br/>miller</span>
+          <span className="navBar-span">
+            brendan
+            <br />
+            miller
+          </span>
           <span className="navBar-span">
             <ColorBar />
           </span>
@@ -19,7 +25,14 @@ export default class NavBar extends Component {
           </span>
         </span>
         <span className="navBar-col navBar-right">
-          <span className="navBar-span">right align</span>
+          <span className="navBar-span">
+            <a href="http://www.github.com/bpatmiller" className="social-icon">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="https://www.linkedin.com/in/bpatmiller/" className="social-icon">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </span>
         </span>
       </div>
     );
